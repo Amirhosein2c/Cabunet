f0e3f0c9e2cdc036dcf35e3ccd04a7b73e0c31b7
amirhosein2c <amirhosein.toosi@gmail.com>
2020-08-13 20:19:04 +0200

    cosmetic changes on script to be readable! :D

--- testIntaloader.py
+++ testIntaloader.py
@@ -2,19 +2,18 @@ from datetime import datetime
 import instaloader
 
 L = instaloader.Instaloader()
-# L.interactive_login("YOUR-USER-ID")
+# L.interactive_login("YOUR-USER-ID")  # You can insert your IG user ID if you like, it will next ask for your password interactively
 
-hashtag = "elevatorselfie"
-#"selfieascensor"
+SINCE = datetime(2015, 8, 10) # From this date  (YYYY, M, D)
+UNTIL = datetime(2020, 8, 10) # To this date
+hashtag = "elevatorselfie"    # Hashtag to search for
+num_posts = 50000             # Max number of links to be retrieved
+
 
 posts = instaloader.Hashtag.from_name(L.context, hashtag).get_posts()
-
-SINCE = datetime(2014, 8, 10)
-UNTIL = datetime(2020, 8, 10)
-
 counter = 0
 post_list = []
-file_object = open(hashtag + "_50000_URLs.txt", 'a+')
+file_object = open(hashtag + "_" + str(num_posts) + "_URLs.txt", 'a+')
 
 userID = None
 for post in posts:
@@ -24,7 +23,7 @@ for post in posts:
     #     userID = user
     if SINCE < postdate < UNTIL:
         counter += 1
-        if counter >= 50000:
+        if counter >= num_posts:
             break
         else:
             post_list.append(post.url)
